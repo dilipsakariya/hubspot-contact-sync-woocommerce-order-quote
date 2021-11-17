8b20600dbc8fa300b3b75758223f219545fdef6e
Dilip Sakariya <sdilip27@gmail.com>
2021-11-17 17:04:59 +0530

    Update V 8

--- hubspot-contact-sync-woocommerce-order-quote.php
+++ hubspot-contact-sync-woocommerce-order-quote.php
@@ -8,7 +8,7 @@
  * Plugin Name:       Hubspot Contact Sync - Woocommerce Order Quote
  * Plugin URI:        https://ducksoupdigital.co.uk/
  * Description:       WooCommerce Sync Contacts and Order or Quote info into Hubspot account using v2 api of hubspot
- * Version:           3.0.0
+ * Version:           8.0.0
  * Author:            DuckSoup Digital 
  * Author URI:        https://ducksoupdigital.co.uk/
  * Text Domain:       hubspot_contact_sync_woocommerce_order_quote
@@ -22,7 +22,7 @@
  */
 
 if ( ! defined( 'ABSPATH') ) {
-	return;
+  return;
 }
 
 
@@ -36,62 +36,81 @@ function hubspot_sync_order_contacts( $order_id ) {
 
     $page_url = site_url('/');
     $page_name = "Home page";
+    $lead_source_description = 'PEL - RFQ/Order';
 
     $order_data = $order->get_data();
     
     if ( $order->has_status('processing') ) {
-	    $page_url = site_url('my-account/view-order/'.$order_id);
-	    $page_name = 'WooCommerce Order Detail Page';
+      $page_url = site_url('my-account/view-order/'.$order_id);
+      $page_name = 'WooCommerce Order Detail Page';
 
-	    $order_notes = $order_data['billing']['wcj_checkout_field_1'];
-	    $order_vat = $order_data['billing']['vat'];
-	}
+      $order_notes = $order_data['billing']['wcj_checkout_field_1'];
+      $order_vat = $order_data['billing']['vat'];
+  }
 
-	if ( $order->has_status('ywraq-new') ) {
-	    $page_url = site_url('my-account/view-quote/'.$order_id);
-	    $page_name = 'WooCommerce Quote Detail Page';
-	    
-	    $order_notes = get_post_meta( $order_id, 'message', true );
-		$order_vat = get_post_meta( $order_id, 'VAT_Number', true );
-	}
+  if ( $order->has_status('ywraq-new') ) {
+      $page_url = site_url('my-account/view-quote/'.$order_id);
+      $page_name = 'WooCommerce Quote Detail Page';
+      $lead_source_description = 'PEL - RFQ/Quote';
+      
+      $order_notes = get_post_meta( $order_id, 'message', true );
+      $order_vat = get_post_meta( $order_id, 'VAT_Number', true );
+  }
 
-	 // The Order data
+   // The Order data
 
-	$order_billing_first_name = $order_data['billing']['first_name'];
+  $order_billing_first_name = $order_data['billing']['first_name'];
   $order_billing_last_name = $order_data['billing']['last_name'];
   $order_billing_company = $order_data['billing']['company'];
   $order_billing_address_1 = $order_data['billing']['address_1'];
   $order_billing_address_2 = $order_data['billing']['address_2'];
-  $order_billing_city = isset($order->get_billing_city())?$order->get_billing_city():$order_data['billing']['city'];
-  $order_billing_state = $order_data['billing']['state'];
-  $order_billing_postcode = $order_data['billing']['postcode'];
-  $order_billing_country = isset($order->get_billing_country())?$order->get_billing_country():$order_data['billing']['country'];
+  // $order_billing_city = isset($order->get_billing_city())?$order->get_billing_city():$order_data['billing']['city'];
+  // $order_billing_state = isset($order->get_billing_state())?$order->get_billing_state():$order_data['billing']['state'];
+  // $order_billing_postcode = isset($order->get_billing_postcode())?$order->get_billing_postcode():$order_data['billing']['postcode'];
+  // $order_billing_country = isset($order->get_billing_country())?$order->get_billing_country():$order_data['billing']['country'];
+
+  $order_billing_city = $order->get_billing_city();
+  $order_billing_state_short = $order->get_billing_state();
+  $order_billing_postcode = $order->get_billing_postcode();
+  $order_billing_country_short = $order->get_billing_country();
+
+  $order_billing_state = WC()->countries->states[$order->get_billing_country()][$order->get_billing_state()];
+  $order_billing_country = WC()->countries->countries[$order->get_billing_country()];
+
+  if($order_billing_country_short == 'UK' || $order_billing_country_short == 'GB'){
+      $order_billing_country = 'United Kingdom';
+  }else if($order_billing_country_short == 'USA' || $order_billing_country_short == 'US'){
+      $order_billing_country = 'United States';
+  }
+
+
+  
   $order_billing_email = $order_data['billing']['email'];
   $order_billing_phone = $order_data['billing']['phone'];
-	
-	$order_total = $order_data['total'];
-	$order_detail = '';
+  
+  $order_total = $order_data['total'];
+  $order_detail = '';
 
-	$count = 1;
-	foreach ($order->get_items() as $item_key => $item ):
-		$product      = $item->get_product(); 
+  $count = 1;
+  foreach ($order->get_items() as $item_key => $item ):
+    $product      = $item->get_product(); 
 
-	    $product_id   = $item->get_product_id();
-	    $variation_id = $item->get_variation_id();
+      $product_id   = $item->get_product_id();
+      $variation_id = $item->get_variation_id();
 
-	    $item_type    = $item->get_type();
+      $item_type    = $item->get_type();
 
-	    $item_name    = $item->get_name();
-	    $quantity     = $item->get_quantity(); 
-	    $product_price  = $product->get_price();
+      $item_name    = $item->get_name();
+      $quantity     = $item->get_quantity(); 
+      $product_price  = $product->get_price();
 
-	    $order_detail .= $count.') '. $item_name .' (#'.$product_id.')'. PHP_EOL;
-	    $count++;
-	endforeach;
+      $order_detail .= $count.') '. $item_name .' (#'.$product_id.')'. PHP_EOL;
+      $count++;
+  endforeach;
 
 
 
-	/* $hs_context stores analytics data about your visitor, including 
+  /* $hs_context stores analytics data about your visitor, including 
     their HubSpot cookie, their IP address, and the page the form was on.
     If you have a custom registration page, you may want to replace the 
     pageUrl and pageName arguments. */
@@ -120,14 +139,18 @@ function hubspot_sync_order_contacts( $order_id ) {
       'order_total' => $order_total,
       'order_detail' => $order_detail,
       'operating_company_pel' => 'true',
+      'leadsource' => 'Website',
+      'lead_source_description_c' => $lead_source_description,
       'hs_context' => json_encode($hs_context)
     );
 
     /* This is the URL we'll be submitting to. Replace "YourPortalID" 
     and "YourFormGUID" with the appropriate values from the previous 
     step. */
-    $portalId = '7760466';
-    $formGuid = '0196b388-43de-4695-9fd8-e4ccfab63d55';
+    $portalId = '123594';
+    // $portalId = '7760466';
+    $formGuid = '48ed3a36-f7c4-42fc-9ea1-23808212f555';
+    // $formGuid = '0196b388-43de-4695-9fd8-e4ccfab63d55';
     
     $endpoint = "https://forms.hubspot.com/uploads/form/v2/$portalId/$formGuid";
 
